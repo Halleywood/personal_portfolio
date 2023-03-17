dd257b7ae84154b334b68ff155327164ff991887
Kelsey Halley <kelseyghalley@gmail.com>
2023-03-16 19:29:08 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function test(){
+    alert("IT WORKS!");
+}
+
+// function changeDisplay(displayID){
+//     let display= `display-${displayID}`;
+//     let upElement = document.getElementById(display);
+//    window.scrollTo(upElement.offsetLeft, upElement.offsetTop);
+
+// }
